775d9ad06d7cc4173f97fadf8e7faa880a441e2c
Abad Hassan <abadkhan71000@gmail.com>
2019-11-03 02:16:32 +0500

    Add files via upload

---
+++ Red.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstractfacttorydemopattren;
+
+/**
+ *
+ * @author fa16-bse-075
+public class Red implements Color {
+	@Override
+	public void fillColor() {
+		System.out.println("Fill Red Color.");
+	}    
+}
